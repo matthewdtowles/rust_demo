956f5a2928d15f233077ace78ee23f31256cd459
matthewdtowles <matthewdtowles@gmail.com>
2024-12-07 01:30:08 -0500

    impl mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-// 1. Given a list of ints, use a vector and return median
-// and also find the mode of the list of ints
-
 // 2. Convert strings to pig latin
 
 // 3. Use hash map and vectors to create a text interface
 // to allow a user to add employee names to a department in a company.
 
-fn median(list: Vec<i32>) -> f64 {
-    // sort list (in place if possible)
-    // return middle number as a float
-    // return mean of middle two numbers if vec size % 2 == 0
+use std::collections::HashMap;
+
+fn median(list: &Vec<i32>) -> f64 {
     let mut sorted = list.clone();
     sorted.sort();
     let size: i32 = sorted.len() as i32;
@@ -21,6 +17,22 @@ fn median(list: Vec<i32>) -> f64 {
     }
 }
 
+fn mode(list: &Vec<i32>) -> i32 {
+    let mut mode = *list.first().unwrap();
+    let mut max = 0;
+    let mut mode_map = HashMap::new();
+    for x in list {
+        *mode_map.entry(x).or_insert(0) += 1;
+    }
+    for key in mode_map.keys() {
+        let cur_val = mode_map.get(key).unwrap();
+        if cur_val > &max {
+            mode = **key;
+            max = *cur_val;
+        }
+    }
+    mode
+}
 fn mean(list: Vec<i32>) -> f64 {
     let mut sum = 0;
     let size = list.len();
@@ -32,7 +44,10 @@ fn mean(list: Vec<i32>) -> f64 {
 
 fn main() {
     let list = vec![59, 37, 7, 23, 23, 43, 41];
-    let median = median(list);
+    let median = median(&list);
     println!("Median: {}", median);
     assert_eq!(median, 37.0);
+    let mode = mode(&list);
+    println!("Mode: {}", mode);
+    assert_eq!(mode, 23);
 }
